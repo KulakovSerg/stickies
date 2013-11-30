f94c3887cfbe91df722ac2d02626cce6c4c8601a
KulakovSerg <kulakov.serg@gmail.com>
2013-11-30 23:41:13 +0400

    rotate works partially

---
+++ app.js
@@ -0,0 +1,19 @@
+/**
+ * Special for SEMrush.
+ * Date: 30.11.13
+ * Time: 17:34
+ *
+ * @author Kuolakov Sergey
+ */
+
+$(function(){
+   $('.sticker').click(function(){
+       var self = $(this),
+           settingsClass = 'show-settings',
+           hasSettings = self.hasClass(settingsClass),
+           getClassName = function(rotate){
+               return ((rotate ? !hasSettings : hasSettings) ? '' : 'not-') + settingsClass;
+           }
+       self.removeClass(getClassName()).addClass(getClassName(true));
+   });
+});
