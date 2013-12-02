c3b74cfc9adddf7c8c795946776b9939f9e180f5
KulakovSerg <kulakov.serg@gmail.com>
2013-12-02 05:02:24 +0400

    sass layout

--- app.js
+++ app.js
@@ -1,22 +1,39 @@
 /**
  * Special for SEMrush.
- * Date: 30.11.13
- * Time: 17:34
- *
  * @author Kuolakov Sergey
  */
 
-$(function(){
-   $('.sticker').click(function(){
-       var self = $(this),
-           settingsClass = 'show-settings',
-           hasSettings = self.hasClass(settingsClass),
-           getClassName = function(rotate){
-               return ((rotate ? !hasSettings : hasSettings) ? '' : 'not-') + settingsClass;
-           }
-       self.removeClass(getClassName()).addClass(getClassName(true));
-       hasSettings && setTimeout(function(){
-           self.removeClass('not-'+settingsClass);
-       },510);
-   });
+$(function () {
+    var wrapperClass = '.wrapper',
+        showCloseButtons = 'showClose',
+        settingsClass = 'show-settings',
+        stickerClass = '.sticker ',
+        duration = 500,
+        toggleSettings = function (e) {
+            var self = $(e.target).closest(stickerClass),
+                hasSettings = self.hasClass(settingsClass),
+                getClassName = function (rotate) {
+                    return ((rotate ? !hasSettings : hasSettings) ? '' : 'not-') + settingsClass;
+                };
+            self.removeClass(getClassName()).addClass(getClassName(true));
+            hasSettings && setTimeout(function () {
+                self.removeClass('not-' + settingsClass);
+            }, duration+10);
+        };
+
+    $(stickerClass+'.more').click(toggleSettings);
+
+    $(stickerClass+'.ready').click(toggleSettings);
+
+    $(stickerClass+'.close').click(function(){
+        var self = $(this);
+        self.css('opacity') > 0 && self.closest(stickerClass).remove();
+    });
+
+    $('.footer .add').click(function () {
+        $('.wrapper').removeClass(showCloseButtons);
+    });
+    $('.footer .remove').click(function () {
+        $('.wrapper').addClass(showCloseButtons);
+    });
 });
